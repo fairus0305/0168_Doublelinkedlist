f79991874956e7d342b3ff572b7a71669a3a04c3
fairus0305 <ruszezzy@gmail.com>
2026-05-01 16:42:16 +0700

    Menambahkan proses pencarian node berdasarkan nomor mahasiswa sebelum penghapusan data

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -95,4 +95,16 @@ public:
         cout << "\nEnter the roll number of the student whose record is to be deleted: ";
         cin >> rollNo;
 
-      
+       Node *current = START;
+
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
+       
